95aa3d6fe2077c5f1564508a3bd30e54ce722f7b
justforfun0906 <jimmy0906abc@gmail.com>
2024-05-20 23:21:11 +0800

    remember all the variable need to be initialized==

--- Scene/ScoreBoardScene.cpp
+++ Scene/ScoreBoardScene.cpp
@@ -6,9 +6,12 @@
 #include "UI/Component/Image.hpp"
 #include "UI/Component/ImageButton.hpp"
 #include "UI/Component/Label.hpp"
-#include "ScoreBoardScene.hpp"
+#include "LoseScene.hpp"
 #include "PlayScene.hpp"
 #include "Engine/Point.hpp"
+#include "Engine/Resources.hpp"
+#include "ScoreBoardScene.hpp"
+
 
 void ScoreBoardScene::Initialize() {
 	// TODO: [HACKATHON-1-SCENE] (1/4): You can imitate the 2 files: 'LoseScene.hpp', 'LoseScene.cpp' to implement your start scene.
@@ -16,14 +19,14 @@ void ScoreBoardScene::Initialize() {
 	int h = Engine::GameEngine::GetInstance().GetScreenSize().y;
 	int halfW = w / 2;
 	int halfH = h / 2;
-	AddNewObject(new Engine::Image("lose/benjamin-happy.png", halfW, halfH, 0, 0, 0.5, 0.5));
-	AddNewObject(new Engine::Label("You Lose :(", "pirulen.ttf", 48, halfW, halfH / 4 + 10, 255, 255, 255, 255, 0.5, 0.5));
+	AddNewObject(new Engine::Label("Leader Board", "pirulen.ttf", 48, halfW, halfH / 4 + 10, 255, 255, 255, 255, 0.5, 0.5));
 	Engine::ImageButton* btn;
 	btn = new Engine::ImageButton("win/dirt.png", "win/floor.png", halfW - 200, halfH * 7 / 4 - 50, 400, 100);
-	btn->SetOnClickCallback(std::bind(&ScoreBoardScene::BackOnClick, this, 2));
+	btn->SetOnClickCallback(std::bind(&ScoreBoardScene::BackOnClick, this, 1));
 	AddNewControlObject(btn);
 	AddNewObject(new Engine::Label("Back", "pirulen.ttf", 48, halfW, halfH * 7 / 4, 0, 0, 0, 255, 0.5, 0.5));
-    bgmInstance = AudioHelper::PlaySample("astronomia.ogg", false, AudioHelper::BGMVolume, PlayScene::DangerTime);
+	//TODO: change the BGM
+	bgmInstance = AudioHelper::PlaySample("select.ogg", true, AudioHelper::BGMVolume);
 }
 void ScoreBoardScene::Terminate() {
 	AudioHelper::StopSample(bgmInstance);
@@ -32,5 +35,5 @@ void ScoreBoardScene::Terminate() {
 }
 void ScoreBoardScene::BackOnClick(int stage) {
 	// Change to select scene.
-	Engine::GameEngine::GetInstance().ChangeScene("stage-select");
+	Engine::GameEngine::GetInstance().ChangeScene("start");
 }
