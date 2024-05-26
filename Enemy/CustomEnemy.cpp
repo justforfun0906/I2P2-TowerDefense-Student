42a159dd5fa34de5c8da5a85633dbcb8d04dc33e
justforfun0906 <jimmy0906abc@gmail.com>
2024-05-26 21:22:56 +0800

    feat: Add CustomBullet class and files for custom turret

--- Enemy/CustomEnemy.cpp
+++ Enemy/CustomEnemy.cpp
@@ -2,6 +2,6 @@
 
 #include "CustomEnemy.hpp"
 
-CustomEnemy::CustomEnemy(int x, int y) : Enemy("play/enemy-4.png", x, y, 10, 50, 10, 100) {
+CustomEnemy::CustomEnemy(int x, int y) : Enemy("play/enemy-6.png", x, y, 10, 50, 50, 100) {
 	// TODO: [CUSTOM-TOOL] You can imitate the 2 files: 'SoldierEnemy.hpp', 'SoldierEnemy.cpp' to create a new enemy.
 }
--- Turret/CustomTurret.cpp
+++ Turret/CustomTurret.cpp
@@ -5,6 +5,7 @@
 #include "Engine/AudioHelper.hpp"
 #include "Bullet/FireBullet.hpp"
 #include "Bullet/LaserBullet.hpp"
+#include "Bullet/CustomBullet.hpp"
 #include "Engine/Group.hpp"
 #include "CustomTurret.hpp"
 #include "Scene/PlayScene.hpp"
@@ -13,7 +14,7 @@
 const int CustomTurret::Price = 100;
 CustomTurret::CustomTurret(float x, float y) :
 	// TODO: [CUSTOM-TOOL] You can imitate the 2 files: 'MachineGunTurret.hpp', 'MachineGunTurret.cpp' to create a new turret.
-	Turret("play/tower-base.png", "play/turret-4.png", x, y, 1000, Price, 0.5) {
+	Turret("play/tower-base.png", "play/turret-4.png", x, y, 500, Price, 1) {
 	// Move center downward, since we the turret head is slightly biased upward.
 	Anchor.y += 8.0f / GetBitmapHeight();
 }
@@ -22,9 +23,7 @@ void CustomTurret::CreateBullet() {
 	float rotation = atan2(diff.y, diff.x);
 	Engine::Point normalized = diff.Normalize();
 	// Change bullet position to the front of the gun barrel.
-	getPlayScene()->BulletGroup->AddNewObject(new FireBullet(Position + normalized * 36, diff, rotation, this));
-	getPlayScene()->BulletGroup->AddNewObject(new FireBullet(Position + normalized * 36, diff, rotation, this));
-	getPlayScene()->BulletGroup->AddNewObject(new FireBullet(Position + normalized * 36, diff, rotation, this));
-	getPlayScene()->BulletGroup->AddNewObject(new FireBullet(Position + normalized * 36, diff, rotation, this));
-	AudioHelper::PlayAudio("laser.wav");
+	getPlayScene()->BulletGroup->AddNewObject(new CustomBullet(Position + normalized * 36, diff, rotation, this));
+	getPlayScene()->BulletGroup->AddNewObject(new CustomBullet(Position + normalized * 36, diff, rotation, this));
+	AudioHelper::PlayAudio("pacman-chomp.ogg");
 }
